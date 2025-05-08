c737fca70956bacffaaa6970e88dbdce13c8382b
Artem Prokop <artem13.prokop@gmail.com>
2025-05-08 12:28:00 +0200

    feat: basic MCP request

---
+++ README.md
@@ -0,0 +1,25 @@
+# Zed Sear Context Server
+
+This extension provides a Model Context Server for SearXNG, for use with the Zed AI assistant.
+
+It adds a `/search` slash command to the Assistant Panel.
+
+## Configuration
+
+To use the extension, you will need to point the context server at a SearXNG instance by setting the `api_url` in your Zed `settings.json`:
+
+```json
+{
+  "context_servers": {
+    "searxng-context-server": {
+      "settings": {
+        "api_url": "http://127.0.0.1:8080"
+      }
+    }
+  }
+}
+```
+
+## Usage
+
+- `/search <query>`: Search web for the given query.
---
+++ src/searxng_model_context.rs
@@ -0,0 +1,73 @@
+use schemars::JsonSchema;
+use serde::Deserialize;
+use std::env;
+use zed::settings::ContextServerSettings;
+use zed_extension_api::{
+    self as zed, serde_json, Command, ContextServerConfiguration, ContextServerId, Project, Result,
+};
+
+const PACKAGE_NAME: &str = "@chipi-chapa-corp/searxng-context-server";
+const SERVER_PATH: &str = "node_modules/@chipi-chapa-corp/searxng-context-server/index.js";
+
+struct SearXNGModelContextExtension;
+
+#[derive(Debug, Deserialize, JsonSchema)]
+struct SearXNGContextServerSettings {
+    api_url: String,
+}
+
+impl zed::Extension for SearXNGModelContextExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn context_server_command(
+        &mut self,
+        _context_server_id: &ContextServerId,
+        project: &Project,
+    ) -> Result<Command> {
+        let latest_version = zed::npm_package_latest_version(PACKAGE_NAME)?;
+        let version = zed::npm_package_installed_version(PACKAGE_NAME)?;
+        if version.as_deref() != Some(latest_version.as_ref()) {
+            zed::npm_install_package(PACKAGE_NAME, &latest_version)?;
+        }
+
+        let settings = ContextServerSettings::for_project("searxng-context-server", project)?;
+        let Some(settings) = settings.settings else {
+            return Err("missing `api_url` setting".into());
+        };
+        let settings: SearXNGContextServerSettings =
+            serde_json::from_value(settings).map_err(|e| e.to_string())?;
+
+        Ok(Command {
+            command: zed::node_binary_path()?,
+            args: vec![env::current_dir()
+                .unwrap()
+                .join(SERVER_PATH)
+                .to_string_lossy()
+                .to_string()],
+            env: vec![("API_URL".into(), settings.api_url)],
+        })
+    }
+
+    fn context_server_configuration(
+        &mut self,
+        _context_server_id: &ContextServerId,
+        _project: &Project,
+    ) -> Result<Option<ContextServerConfiguration>> {
+        let installation_instructions =
+            include_str!("../configuration/installation_instructions.md").to_string();
+        let default_settings = include_str!("../configuration/default_settings.jsonc").to_string();
+        let settings_schema =
+            serde_json::to_string(&schemars::schema_for!(SearXNGContextServerSettings))
+                .map_err(|e| e.to_string())?;
+
+        Ok(Some(ContextServerConfiguration {
+            installation_instructions,
+            default_settings,
+            settings_schema,
+        }))
+    }
+}
+
+zed::register_extension!(SearXNGModelContextExtension);
